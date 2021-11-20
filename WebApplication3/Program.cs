45e6858382f18ab1e20fed2896f0f8c84b15e747
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2021-11-21 00:31:39 +0200

    clent

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -5,14 +5,47 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebApplication3
 {
     public class Program
     {
-        public static void Main(string[] args)
+        const string baseURL = "http://localhost:8000";
+        public static async Task Main(string[] args)
         {
+            using(var httpClient=new HttpClient())
+            {
+                swaggerClient restClient = new swaggerClient(baseURL, httpClient);
+                //get all product
+                var products=await restClient.CatalogAllAsync();
+                foreach(var product in products)
+                {
+                    Console.WriteLine(product.Id+"  "+
+                        product.Name + "  "+
+                        product.Price + "  ");
+                }
+                //post product
+                try
+                {
+                    var newProduct = await restClient.CatalogAsync(
+                        new Product
+                        {
+                            Name = "product 1",
+                            Price = 500,
+                            Category = "clothes",
+                            Description = "aaaaa"
+                        });
+
+                   
+                }
+                catch 
+                {
+
+                }
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
